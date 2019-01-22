944fb2ac7f2fb766ce6ae152c4365300edb925af
simonezana99@gmail.com <simonezana99@gmail.com>
2019-01-22 18:29:25 +0100

    funge

--- public/js/main.js
+++ public/js/main.js
@@ -8,12 +8,11 @@ function bookSearch() {
         dataType: "json",
 
         success: function(data){
-            console.log(data)
+            for (i=0;i<data.items.length;i++){
+                results.innerHTML += "<h2>" + data.items[i].volumeInfo.title + "</h2>" + "<img src=" + data.items[i].volumeInfo.imageLinks.smallThumbnail + ">"
+            }
         },
         type: 'GET'
     });
 }
 document.getElementById('button').addEventListener('click',bookSearch,false)
-//AJAX è l'acronimo di Asynchronous Javascript And XML e indica una tecnica per permettere di costruire applicazioni asincrone,
-//in cui in buona sostanza, non viene ricaricata ogni volta tutta la pagina,
-//bensì solo una “zona”, che è stata “comunicata” come parametro alla funzione: $.ajax.
