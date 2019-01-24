e17abbd52feb1a92796a9ba4cdd77b1ca8189227
simonezana99@gmail.com <simonezana99@gmail.com>
2019-01-24 10:45:33 +0100

    gestita descrizione non disponibile

--- public/js/main.js
+++ public/js/main.js
@@ -10,12 +10,24 @@ function bookSearch() {
         success: function(data){
             for (i=0;i<data.items.length;i++){
                 console.log(data.items[i]);
-                if(i==0){
-                          results.innerHTML += "<h2 class = 'primo'>" + data.items[i].volumeInfo.title + "</h2>" + "<img src=" + data.items[i].volumeInfo.imageLinks.thumbnail  + ">" + "<p class='description'>"  + data.items[i].volumeInfo.description + "</p>"
-                        }
-                else {
-                      results.innerHTML += "<h2 class = 'altri'>" + data.items[i].volumeInfo.title + "</h2>" + "<img src=" + data.items[i].volumeInfo.imageLinks.thumbnail  + ">" + "<p class='description'>"  + data.items[i].volumeInfo.description + "</p>"
-                     }
+                if (data.items[i].volumeInfo.description == undefined) {
+                    if(i==0){
+                              results.innerHTML += "<h2 class = 'primo'>" + data.items[i].volumeInfo.title + "</h2>" + "<h4>&nbsp;&nbsp;&nbsp;&nbsp;" + data.items[i].volumeInfo.authors[0]+ "</h4>" + "<img src=" +   data.items[i].volumeInfo.imageLinks.thumbnail  + ">" + "<p class='description'>"  + "descrizione non disponibile." + "</p>"
+                            }
+                    else {
+                          results.innerHTML += "<h2 class = 'altri'>" + data.items[i].volumeInfo.title + "</h2>" + "</h2>" + "<h4>&nbsp;&nbsp;&nbsp;&nbsp;" + data.items[i].volumeInfo.authors[0]+ "</h4>" + "<img src=" +  data.items[i].volumeInfo.imageLinks.thumbnail  + ">" + "<p class='description'>"  + "descrizione non disponibile." + "</p>"
+                         }
+                }
+                else{
+                    if(i==0){
+                              results.innerHTML += "<h2 class = 'primo'>" + data.items[i].volumeInfo.title + "</h2>" + "<h4>&nbsp;&nbsp;&nbsp;&nbsp;" + data.items[i].volumeInfo.authors[0]+ "</h4>" + "<img src=" +   data.items[i].volumeInfo.imageLinks.thumbnail  + ">" + "<p class='description'>"  + data.items[i].volumeInfo.description + "</p>"
+                            }
+                    else {
+                          results.innerHTML += "<h2 class = 'altri'>" + data.items[i].volumeInfo.title + "</h2>" + "</h2>" + "<h4>&nbsp;&nbsp;&nbsp;&nbsp;" + data.items[i].volumeInfo.authors[0]+ "</h4>" + "<img src=" +  data.items[i].volumeInfo.imageLinks.thumbnail  + ">" + "<p class='description'>"  + data.items[i].volumeInfo.description + "</p>"
+                         }
+                }
+
+
             }
         },
         type: 'GET'
