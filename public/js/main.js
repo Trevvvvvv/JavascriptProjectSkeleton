e4e7d8bb77d91a03e2911637e8bc0c38b9d2039d
simonezana99@gmail.com <simonezana99@gmail.com>
2019-01-24 12:14:45 +0100

    done

--- public/js/main.js
+++ public/js/main.js
@@ -1,7 +1,6 @@
 function bookSearch() {
     var search = document.getElementById('search').value
     document.getElementById('results').innerHTML = ""
-    console.log('anzoni')
     console.log(search)
     $.ajax({
         url:"https://www.googleapis.com/books/v1/volumes?q=" + search,
