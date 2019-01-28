2ccfab41778b3f250c11b64e1db2eb1d2aa67e34
simonezana99@gmail.com <simonezana99@gmail.com>
2019-01-28 08:45:06 +0100

    aggiunto buy link

--- public/js/main.js
+++ public/js/main.js
@@ -7,27 +7,36 @@ function bookSearch() {
         dataType: "json",
 
         success: function(data){
-            for (i=0;i<data.items.length;i++){
-                console.log(data.items[i]);
-                if (data.items[i].volumeInfo.description == undefined) {
-                    if(i==0){
-                              results.innerHTML += "<h2 class = 'primo'>" + data.items[i].volumeInfo.title + "</h2>" + "<h4>&nbsp;&nbsp;&nbsp;&nbsp;" + data.items[i].volumeInfo.authors[0]+ "</h4>" + "<img src=" +   data.items[i].volumeInfo.imageLinks.thumbnail  + ">" + "<p class='description'>"  + "descrizione non disponibile." + "</p>"
-                            }
-                    else {
-                          results.innerHTML += "<h2 class = 'altri'>" + data.items[i].volumeInfo.title + "</h2>" + "</h2>" + "<h4>&nbsp;&nbsp;&nbsp;&nbsp;" + data.items[i].volumeInfo.authors[0]+ "</h4>" + "<img src=" +  data.items[i].volumeInfo.imageLinks.thumbnail  + ">" + "<p class='description'>"  + "descrizione non disponibile." + "</p>"
-                         }
-                }
-                else{
-                    if(i==0){
-                              results.innerHTML += "<h2 class = 'primo'>" + data.items[i].volumeInfo.title + "</h2>" + "<h4>&nbsp;&nbsp;&nbsp;&nbsp;" + data.items[i].volumeInfo.authors[0]+ "</h4>" + "<img src=" +   data.items[i].volumeInfo.imageLinks.thumbnail  + ">" + "<p class='description'>"  + data.items[i].volumeInfo.description + "</p>"
-                            }
-                    else {
-                          results.innerHTML += "<h2 class = 'altri'>" + data.items[i].volumeInfo.title + "</h2>" + "</h2>" + "<h4>&nbsp;&nbsp;&nbsp;&nbsp;" + data.items[i].volumeInfo.authors[0]+ "</h4>" + "<img src=" +  data.items[i].volumeInfo.imageLinks.thumbnail  + ">" + "<p class='description'>"  + data.items[i].volumeInfo.description + "</p>"
-                         }
-                }
-
-
+          for (i=0;i<data.items.length;i++){
+            console.log(data.items[i]);
+            if (data.items[i].volumeInfo.description == undefined) {
+              if(i==0)
+              {
+                results.innerHTML += "<h2 class = 'primo'>" + data.items[i].volumeInfo.title + "</h2>" + "<h4>&nbsp;&nbsp;&nbsp;&nbsp;" + data.items[i].volumeInfo.authors[0]+ "</h4>" + "<img src=" +   data.items[i].volumeInfo.imageLinks.thumbnail  + ">" + "<p class='description'>"  + "descrizione non disponibile." + "</p>"
+              }
+              else
+              {
+                results.innerHTML += "<h2 class = 'altri'>" + data.items[i].volumeInfo.title + "</h2>" + "</h2>" + "<h4>&nbsp;&nbsp;&nbsp;&nbsp;" + data.items[i].volumeInfo.authors[0]+ "</h4>" + "<img src=" +  data.items[i].volumeInfo.imageLinks.thumbnail  + ">" + "<p class='description'>"  + "descrizione non disponibile." + "</p>"
+              }
             }
+            else
+            {
+              if(i==0)
+                {
+                  results.innerHTML += "<h2 class = 'primo'>" + data.items[i].volumeInfo.title + "</h2>" + "<h4>&nbsp;&nbsp;&nbsp;&nbsp;" + data.items[i].volumeInfo.authors[0]+ "</h4>" + "<img src=" +   data.items[i].volumeInfo.imageLinks.thumbnail  + ">" + "<p class='description'>"  + data.items[i].volumeInfo.description + "</p>"
+                }
+                else
+                {
+                  results.innerHTML += "<h2 class = 'altri'>" + data.items[i].volumeInfo.title + "</h2>" + "</h2>" + "<h4>&nbsp;&nbsp;&nbsp;&nbsp;" + data.items[i].volumeInfo.authors[0]+ "</h4>" + "<img src=" +  data.items[i].volumeInfo.imageLinks.thumbnail  + ">" + "<p class='description'>"  + data.items[i].volumeInfo.description + "</p>"
+                }
+            }
+            if (data.items[i].saleInfo.buyLink != undefined)
+            {
+            results.innerHTML += "<span class = 'description' style ='float:right'>Per comprare il libro clicca qui: " + "<a href ='" + data.items[i].saleInfo.buyLink + "'>" + "<button> compra il libro </button>" + "</span>"
+            }
+          }
+
+
         },
         type: 'GET'
     });
